e400603af1fea5dba810239cf108db8ea314a075
Sergio Falcon <sgzfalcon@gmail.com>
2018-03-13 11:34:08 +0100

    Add two representations (words per message and user)

--- whatsappAnalyzer/README.md
+++ whatsappAnalyzer/README.md
@@ -2,10 +2,12 @@
 The objective of this script is to provide insightful information about your way of communicate in the mobile app WhatsApp (Android & iOS).
 ## Features completed
 - Number of messages sent by participant.
-## Planned features
 - Number of words sent by participant.
+- Number of words per message.
+## Planned features
 - Hours of the day with more activity.
 - Mentions of participants.
 - Read files with 12 hour format.
 - Means and medians for different variables.
+- ~~Number of words sent by participant.~~ (Done)
 ## Known bugs
--- whatsappAnalyzer/analyzer.py
+++ whatsappAnalyzer/analyzer.py
@@ -20,18 +20,47 @@ if chatFile.read(1) != '[': #Android
     chatFile.seek(0)
 else: #IOS
     regex = re.compile(r'(\[\d+/\d+/\d+\s\d+:\d+:\d+)\]\s([^:]*):\s(.*)')
-    chatFile.seek(1)
 results = regex.findall(chatFile.read())
 chatFile.close()
-chatFrame = pd.DataFrame(results, columns=['Datetime','Sender','Message'])
-
+chat = pd.DataFrame(results, columns=['Datetime','Sender','Message'])
+#Clean data
+chatClean = chat.copy()
+chatClean['Message'] = chatClean['Message'].replace(regex=True,
+    to_replace=r'<.\w+\s\w+>', value = r'') #Replace omitted files
+punctuation = ['¿','?','!','.','¡','(',')','"']
+chatClean['Message'] = chatClean['Message'].replace(to_replace=punctuation,
+    value = '') #Replace punctuation
 #Analysis
+fig1 = plt.figure()
 #Senders
+#Number of Messages
 senders = {}
-for sender in chatFrame['Sender'].unique():
-    senders[sender] = chatFrame[chatFrame['Sender']==sender]['Sender'].count()
-sendersFrame = pd.DataFrame(list(senders.items()),columns=['Sender','Messages']) \
-                .sort_values('Messages')
-ax = sns.barplot(x='Sender',y='Messages',data=sendersFrame.iloc[:-20])
-ax.set_xticklabels(ax.get_xticklabels(), fontsize=7)
+for sender in chat['Sender'].unique():
+    senders[sender] = chat[chat['Sender']==sender]['Sender'].count()
+sendersDF = pd.DataFrame(list(senders.items()),columns=['Sender','Messages'])
+fig1.add_subplot(2,2,1)
+ax1 = sns.barplot(x='Sender',y='Messages',
+    data=sendersDF.sort_values('Messages'))
+ax1.set_xticklabels(ax1.get_xticklabels(), fontsize=7)
+#Words by sender
+senders = {}
+for sender in chatClean['Sender'].unique():
+    wordsList = chatClean[chatClean['Sender']==sender]['Message'].tolist()
+    senders[sender] = ' '.join(wordsList).lower().split()
+sendersDF['Words'] = senders.values()
+#wordsBySender = pd.DataFrame(list(senders.items()),columns=['Sender','Words'])
+for sender in senders.keys():
+    senders[sender] = len(senders[sender])
+sendersDF['Number of Words'] = senders.values()
+sendersDF['Words per message'] = sendersDF['Number of Words']/sendersDF['Messages']
+fig1.add_subplot(2,2,2)
+ax2 = sns.barplot(x='Sender',y='Number of Words',
+    data=sendersDF.sort_values('Number of Words'))
+ax2.set_xticklabels(ax2.get_xticklabels(), fontsize=7)
+
+fig1.add_subplot(2,2,3)
+ax3 = sns.barplot(x='Sender',y='Words per message',
+    data=sendersDF.sort_values('Words per message'))
+ax3.set_xticklabels(ax3.get_xticklabels(), fontsize=7)
+
 plt.show()
