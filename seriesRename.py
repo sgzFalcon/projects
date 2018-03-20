1ab34a1fc8e6744c3936b6c348c94cfdaa269e39
Sergio Falcon <sgzfalcon@gmail.com>
2018-03-20 10:28:23 +0100

    Clean code

--- seriesRename.py
+++ seriesRename.py
@@ -6,16 +6,16 @@ else:
     season = sys.argv[1]
 filesRenamed = 0
 extension = ['.avi','.mkv','.mp4']
-index = 0
+index1 = 0
+index2 = 0
 for filename in filenames:
     if filename[-4:] in extension:
-        index += 1
-        os.rename(filename, season + 'x' + '%02d' % (index) + filename[-4:])
+        index1 += 1
+        os.rename(filename, season + 'x' + '%02d' % (index1) + filename[-4:])
         filesRenamed += 1
-index = 0
-for filename in filenames:
     if '.srt' in filename:
-        index += 1
-        os.rename(filename, season + 'x' + '%02d' % (index) + '.en' + extension)
+        index2 += 1
+        os.rename(filename, season + 'x' + '%02d' % (index2) + '.en.srt')
         filesRenamed += 1
+
 print('{num:d} files renamed'.format(num=filesRenamed) )
